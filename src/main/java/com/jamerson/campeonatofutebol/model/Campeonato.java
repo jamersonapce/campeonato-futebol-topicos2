35ae879f9ad1ae63fe4ae4fad6354d38a4b285c4
Jamerson Silva <jamerson.silva@acad.ifma.edu.br>
2018-05-21 18:06:41 -0300

    modelando a entidade campeonato

---
+++ src/main/java/com/jamerson/campeonatofutebol/model/Campeonato.java
@@ -0,0 +1,58 @@
+package com.jamerson.campeonatofutebol.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table (name = "campeonato")
+public class Campeonato implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+
+    @NotBlank
+    private String nome;
+
+
+    @NotBlank
+    private Integer ano;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getAno() {
+        return ano;
+    }
+
+    public void setAno(Integer ano) {
+        this.ano = ano;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Campeonato that = (Campeonato) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id);
+    }
+}
